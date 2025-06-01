825c82a099b9ee90ad778020a2f45b6711ab6342
dflynn93 <74905665+dflynn93@users.noreply.github.com>
2025-06-01 22:19:05 +0100

    Update VotingContract.js
    changes altered to work with backend - Voting.sol

--- VotingContract.js
+++ VotingContract.js
@@ -2,9 +2,16 @@ import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@6.6.2/+esm";
 import abi from './ABI.json' assert { type: "json" };
 
 const contractAddress = "0x1Da5916E8443b0f028d2bdA63b8639eF609e9bDe";
+const contractABI = abi;
 
-export const getContract = async (signer) => {
-    return new ethers.Contract(contractAddress, abi, signer);
+export function getContract(providerOrSigner) {
+    try {
+        return new ethers.Contract(contractAddress, contractABI, providerOrSigner);
+    }
+    catch (error) {
+        console.error("Error getting contract:", error);
+        return null;
+    }
 };
 
 export const getCandidates = async (contract) => {
@@ -18,7 +25,8 @@ export const getCandidates = async (contract) => {
 };
 
 export const hasUserVoted = async (contract, address) => {
-    return await contract.hasVoted(address);
+    const voted = await contract.hasVotedStatus(address);
+    return voted;
 };
 
 export const voteForCandidate = async (contract, id) => {
